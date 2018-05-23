8965cf980dce2fdd950ae2bc6e5c5df85d9615ce
sdasd30 <danielzhao2@gmail.com>
2018-05-23 09:36:49 -0400

    Added animations, looks better now

--- Assets/Scripts/Player/MvmtBody.cs
+++ Assets/Scripts/Player/MvmtBody.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class MvmtBody : MonoBehaviour {
 	public float walkSpeed;
-	private Rigidbody2D mbody;
-	public bool moving;
-	// Use this for initialization
-	void Start () {
-		mbody = GetComponent<Rigidbody2D> ();
+	[HideInInspector] public bool moving;
+	Rigidbody2D mBody;
+	AnimatorSprite mAnim;
+
+	void Start(){
+		mBody = GetComponent<Rigidbody2D> ();
+		mAnim = GetComponent<AnimatorSprite> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		Vector3 vel = new Vector3 ();
@@ -22,7 +24,6 @@ public class MvmtBody : MonoBehaviour {
 		moving = false;
 
 		if (Input.GetKey ("w")) {
-			Debug.Log ("UP");
 			vel.y += 1;
 			up = true;
 			moving = true;
@@ -49,7 +50,7 @@ public class MvmtBody : MonoBehaviour {
 			vel = Vector3.Normalize (vel);
 			vel *= walkSpeed;
 			vel *= Time.deltaTime;
-			mbody.transform.Translate (vel);
+			mBody.transform.Translate (vel);
 			if (up) {
 				angle = 0;
 				if (right)
@@ -81,6 +82,9 @@ public class MvmtBody : MonoBehaviour {
 			float angle = Mathf.Atan2 (mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 			angle -= 90;
 			transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle));
+			mAnim.Play ("PlayerIdle");
+		} else {
+			mAnim.Play ("PlayerWalk");
 		}
 				
 	}
